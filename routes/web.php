53cb19fe5a39170f5a65b750f4ec419414327e42
Hazem Mohamed <hazem.fcih12@gmail.com>
2021-07-27 14:42:05 +0200

    website subdomain test2

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::domain('{subdomain}.'.config('app.short_url'))->group(function ($subdomain) {
+    Route::get('/', function ($subdomain) {
+        return $subdomain;
+});
 Route::get('/', function () {
     return view('LandingPage.index');
 })->name('welcome');
@@ -30,9 +33,7 @@ Route::resource('podcasts',\App\Http\Controllers\PodcastController::class);
 Route::resource('podcasts.about', \App\Http\Controllers\PodcastAboutController::class)->only('index');
 Route::resource('podcasts.episode', \App\Http\Controllers\EpisodeController::class)->only('show');
 Route::resource('podcasts.faqs', \App\Http\Controllers\PodcastFaqsController::class)->only('index');
-Route::domain('{subdomain}.'.config('app.short_url'))->group(function ($subdomain) {
- return $subdomain;
-});
+
 Route::prefix('dashboard')->middleware('auth')->name('dashboard.')->group(function () {
 
     Route::get('/home',\App\Http\Controllers\PodcastDashboardController::class.'@index')->name('home');
